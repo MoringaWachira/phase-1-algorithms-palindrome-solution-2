a2a3dbdbedd01e492d8f03892eed7c1c119e6dc7
first-name second-name <silvia.wachira@student.moringa.com>
2022-08-28 18:39:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++){
+    const endIndex = word.length - 1 -startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+    return false;
+    }
+  }
+  return true;
 }
+  // // Write your algorithm here
+  // will need to make isPalindrome() return true or false
+  // if inputed string is the same forward and backward
+  // then return true if not return false 
 
 /* 
   Add your pseudocode here
+  iterate from beginning of the string to the middle
+  compare the letter we are iterating over to the corresponding letter at the end of the string
+  if the letters don't match return false
+  if we get to the middle and they do return true 
 */
 
 /*
